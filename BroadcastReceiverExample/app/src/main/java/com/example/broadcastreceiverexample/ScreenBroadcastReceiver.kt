399f858e4e19faec0521e65617275cfdc4dba756
yu04038 <yu04038@gmail.com>
2022-01-18 01:21:35 +0900

    setup broadcast receiver

---
+++ BroadcastReceiverExample/app/src/main/java/com/example/broadcastreceiverexample/ScreenBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.broadcastreceiverexample
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ScreenBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("ScreenBroadcastReceiver.onReceive() is not implemented")
+    }
+}
