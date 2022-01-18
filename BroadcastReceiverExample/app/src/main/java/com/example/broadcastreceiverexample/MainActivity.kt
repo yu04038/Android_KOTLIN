133fdab9af25597fcc9e89496aac5e28e7d15567
yu04038 <yu04038@gmail.com>
2022-01-18 22:35:54 +0900

    broadcast receiver 구현

--- BroadcastReceiverExample/app/src/main/java/com/example/broadcastreceiverexample/MainActivity.kt
+++ BroadcastReceiverExample/app/src/main/java/com/example/broadcastreceiverexample/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.broadcastreceiverexample
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +10,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 브로드캐스트 리시버의 서브 클래스 객체 생성
+        val br: BroadcastReceiver = ScreenBroadcastReceiver()
+
+        // IntentFilter()를 이용하여 브로드캐스트 중 수신하고 싶은 메세지를 addAction()을 이용하여 추가한다.
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_SCREEN_ON)
+        }
+
+        // 그리고 registerReceiver()를 이용하여 브로드캐스트 리시버를 등록한다.
+        registerReceiver(br, filter)
     }
 }
--- BroadcastReceiverExample/app/src/main/java/com/example/broadcastreceiverexample/ScreenBroadcastReceiver.kt
+++ BroadcastReceiverExample/app/src/main/java/com/example/broadcastreceiverexample/ScreenBroadcastReceiver.kt
@@ -3,11 +3,18 @@ package com.example.broadcastreceiverexample
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
 
 class ScreenBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("ScreenBroadcastReceiver.onReceive() is not implemented")
+        // 화면이 켜졌을 때, "good"이라는 토스트 메세지를 출력하도록 명시
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+            Log.e("broadcast", "good")
+            var toast = Toast.makeText(context, "good", Toast.LENGTH_LONG)
+            toast.show()
+        }
     }
 }
