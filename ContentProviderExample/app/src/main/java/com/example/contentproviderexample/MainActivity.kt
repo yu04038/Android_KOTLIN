6297904ba9e707fada80a5627803260637d955f4
yu04038 <yu04038@gmail.com>
2022-01-20 00:50:11 +0900

    Contact content provider

--- ContentProviderExample/app/src/main/java/com/example/contentproviderexample/MainActivity.kt
+++ ContentProviderExample/app/src/main/java/com/example/contentproviderexample/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.example.contentproviderexample
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.SearchView
+import android.widget.SimpleCursorAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.example.contentproviderexample.databinding.ActivityMainBinding
+import org.jetbrains.annotations.Contract
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    var cols = listOf<String>(
+        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+        ContactsContract.CommonDataKinds.Phone.NUMBER,
+        ContactsContract.CommonDataKinds.Phone._ID
+    ).toTypedArray()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, Array(1){Manifest.permission.READ_CONTACTS}, 111)
+        } else {
+            readContact()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            readContact()
+    }
+
+    private fun readContact() {
+        var from = listOf<String>(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER,
+            ContactsContract.CommonDataKinds.Phone._ID).toTypedArray()
+
+        var to = intArrayOf(android.R.id.text1, android.R.id.text2)
+        var rs = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                        cols, null, null,
+                                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+
+        var adapter = SimpleCursorAdapter(this, android.R.layout.simple_list_item_2,
+                                            rs, from, to, 0)
+
+        binding.listView1.adapter = adapter
+        binding.searchView.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                rs = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    cols, "${ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME} Like ?",
+                    Array(1){"%$p0%"},
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+                
+                adapter.changeCursor(rs)
+                return false
+            }
+        })
     }
 }
