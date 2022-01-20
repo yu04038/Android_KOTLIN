7c9db686ff89694f9cad2b264fec7f20a4df2ecf
yu04038 <yu04038@gmail.com>
2022-01-21 05:32:58 +0900

    암시적 인텐트

--- ImplicitIntentExample/app/src/main/java/com/example/implicitintentexample/MainActivity.kt
+++ ImplicitIntentExample/app/src/main/java/com/example/implicitintentexample/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.implicitintentexample
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    val button: Button by lazy { findViewById<Button>(R.id.button) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW)
+            var uri = Uri.parse("content://sdcard/Download/music.mp3")
+            intent.setDataAndType(uri, "audio/mp3")
+
+            // Chooser 생성
+            val chooser = Intent.createChooser(intent, "Open with")
+
+            startActivity(chooser)
+        }
     }
 }
