637f3450bac7e1bebce1fa2c9de073b53848b0a9
yu04038 <yu04038@gmail.com>
2022-01-28 01:33:05 +0900

    companion object

--- KotlinSample/app/src/main/java/com/example/kotlinsample/CompanionObject.kt
+++ KotlinSample/app/src/main/java/com/example/kotlinsample/CompanionObject.kt
@@ -2,9 +2,30 @@ package com.example.kotlinsample
 
 class Book private constructor(val id: Int, val name: String) {
 
-    companion object {
+    // private property 나 메소드를 읽어올 수 있게 끔 해준다.
+    // static method, static property
+    companion object BookFactory : IdProvider{
+
+        val myBook = "new Book"
+
         fun create() = Book(0, "animal farm")
+
+        fun createB() = Book(getId(), myBook)
+
+        override fun getId(): Int {
+            return 444
+        }
     }
 }
 
-fun main() {}
+interface IdProvider {
+    fun getId() : Int
+}
+
+fun main() {
+    val book = Book.create()
+    val bookB = Book.createB()
+
+    println("${book.id}, ${book.name}")
+    println("${bookB.id}, ${bookB.name}")
+}
---
+++ KotlinSample/app/src/main/java/com/example/kotlinsample/Sample3.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinsample
+
+class Sample3 {
+}
