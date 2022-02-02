40b93dffc02a91e776c737b1132191552534e469
yu04038 <yu04038@gmail.com>
2022-02-02 21:02:12 +0900

    Update Sample3.kt

--- KotlinSample/app/src/main/java/com/example/kotlinsample/Sample3.kt
+++ KotlinSample/app/src/main/java/com/example/kotlinsample/Sample3.kt
@@ -1,4 +1,27 @@
 package com.example.kotlinsample
 
-class Sample3 {
+import com.example.kotlinsample.CarFactory.cars
+
+// Singleton 패턴 적용
+// CarFactory 객체를 한번 생성하고 다시는 생성하지 않는다.
+// 어디서 사용하든간에 처음 컴파일 될 때 만들어지는 instance 딱 하나이다. 
+object CarFactory {
+    val cars = mutableListOf<Car>()
+    fun makeCar(horsePower: Int): Car {
+        val car = Car(horsePower)
+        cars.add(car)
+
+        return car
+    }
+}
+
+data class Car(var horsePower:Int)
+
+fun main() {
+    val car1 = CarFactory.makeCar(10)
+    val car2 = CarFactory.makeCar(200)
+
+    println(car1)
+    println(car2)
+    println(cars.size.toString())
 }
