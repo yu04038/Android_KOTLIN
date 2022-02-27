9016ea37c7169835bd6cc9c4d4d9c812ad663683
yu04038 <yu04038@gmail.com>
2022-02-27 16:38:11 +0900

    프로젝트 이름 변경

--- Weather/app/src/main/java/com/example/weather/MainActivity.kt
+++ Weather/app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ourtodo
+package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- Weather/app/src/test/java/com/example/weather/ExampleUnitTest.kt
+++ Weather/app/src/test/java/com/example/weather/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.ourtodo
+package com.example.weather
 
 import org.junit.Test
 
