d3fbb257e57058439db9511d019bb511a78c4f19
yu04038 <yu04038@gmail.com>
2022-03-06 18:02:13 +0900

    경도, 위도 업데이트 20초

--- WeatherExample/app/src/main/java/com/example/weatherexample/MainActivity.kt
+++ WeatherExample/app/src/main/java/com/example/weatherexample/MainActivity.kt
@@ -18,23 +18,28 @@ class MainActivity : AppCompatActivity() {
 
     private var mFusedLocationProviderClient: FusedLocationProviderClient? = null // 현재 위치를 가져오기 위한 변수
     lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
-    internal lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장
+    private lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장
     private val REQUEST_PERMISSION_LOCATION = 10
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        val view = binding.root
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
 
         mLocationRequest =  LocationRequest.create().apply {
-            interval = 2000
+            interval = 1000 * 20
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
 
         if (checkPermissionForLocation(this)) {
+            Log.e("onstart", "onstart")
             startLocationUpdates()
         }
     }
@@ -61,9 +66,8 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == REQUEST_PERMISSION_LOCATION) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 startLocationUpdates()
-
             } else {
-                Log.d("ttt", "onRequestPermissionsResult() _ 권한 허용 거부")
+                Log.e("ttt", "onRequestPermissionsResult() _ 권한 허용 거부")
                 Toast.makeText(this, "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show()
             }
         }
@@ -71,14 +75,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun startLocationUpdates() {
 
-        //FusedLocationProviderClient의 인스턴스를 생성.
+        Log.e("locationUpdate", "startLocationUpdates()")
+
+        //FusedLocationProviderClient 의 인스턴스를 생성.
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return
+                Log.e("locationUpdate", "두 위치 권한 중 하나라도 없는 경우")
+                return
         }
+
         // 기기의 위치에 관한 정기 업데이트를 요청하는 메서드 실행
         // 지정한 루퍼 스레드(Looper.myLooper())에서 콜백(mLocationCallback)으로 위치 업데이트를 요청
+        Log.e("locationUpdate", "두 위치 권한 중 하나라도 존재하는 경우")
         Looper.myLooper()?.let {
             mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback,
                 it
@@ -88,16 +97,21 @@ class MainActivity : AppCompatActivity() {
 
     // 시스템으로 부터 위치 정보를 콜백으로 받음
     private val mLocationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
+        override fun onLocationResult(p0: LocationResult) {
             // 시스템에서 받은 location 정보를 onLocationChanged()에 전달
-            locationResult.lastLocation
-            onLocationChanged(locationResult.lastLocation)
+            p0.lastLocation
+            onLocationChanged(p0.lastLocation)
         }
     }
+
 
     // 시스템으로 부터 받은 위치정보를 화면에 갱신해주는 메소드
     fun onLocationChanged(location: Location) {
         mLastLocation = location
         Log.e("위도, 경도", mLastLocation.latitude.toString() + ", " + mLastLocation.longitude.toString())
+    }
+
+    private fun stopLocationUpdates() {
+        mFusedLocationProviderClient!!.removeLocationUpdates(mLocationCallback)
     }
 }
